9f24194e246602dda213912fc294194cb5e822c6
muhwezideo <aggrey256@gmail.com>
2020-10-19 19:45:10 +0300

    ft-set-up-store

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,15 @@ import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import './App.css';
+
+
 
 function App() {
   return (
+    <Provider store = { store }>
     <Router>
     <div className="App">
       <Navbar/>
@@ -20,6 +25,7 @@ function App() {
       <Footer/>
     </div>
     </Router>
+    </Provider>
   );
 }
 
